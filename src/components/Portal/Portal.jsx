fcc51781b56437b9ddcc895282c659e31d92b2c0
hasannhameed <142923441+hasannhameed@users.noreply.github.com>
2025-04-03 11:24:17 +0530

    nothuing

--- src/components/Portal/Portal.jsx
+++
@@ -1,9 +0,0 @@
-
-import ReactDOM from "react-dom";
-
-const Portal = ({children}) => {
-    const portalRoot = document.getElementById('portal-root');
-    return ReactDOM.createPortal(children,portalRoot);
-}
-
-export default Portal;
